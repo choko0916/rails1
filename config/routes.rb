39645528e881008ba02685fbdaeea250001441af
中井　佑香 <yuka@nakaiyuukanoMacBook-Air.local>
2022-02-21 01:36:26 +0900

    cannot load such file -- sassc

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  get 'lessons/hello', to: 'lessons#hello'
+  get 'lessons/call', to: 'lessons#call'
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resources :users
+end
